739bc031866b5ada65a0e2c566e3bc8b8fbc84e2
KercyDing <dkx215417@gmail.com>
2026-02-28 20:27:07 +0800

    chore: replace release download base URL

--- .vitepress/version.ts
+++ .vitepress/version.ts
@@ -5,7 +5,7 @@ export const VERSION = '1.0.1'
 export const RELEASE_TAG = 'v1.0.1'
 
 /** GitHub Release 下载基础 URL */
-export const RELEASE_BASE = `https://github.com/SeaLantern-Studio/SeaLantern/releases/download/${RELEASE_TAG}`
+export const RELEASE_BASE = `https://cnb.cool/SeaLantern-studio/SeaLantern/-/releases/download/${RELEASE_TAG}`
 
 /** 安装包资产版本（受上游打包命名影响，可能与 VERSION 不一致） */
 export const ASSET_VERSION = '1.0.0-1'
